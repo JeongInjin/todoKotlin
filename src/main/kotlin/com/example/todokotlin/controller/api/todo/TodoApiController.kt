6917fc27699fd97219e2fc210fac920939307d59
injinjeong <snowwwwww@naver.com>
2021-12-29 14:22:36 +0900

    kotlin commit

--- src/main/kotlin/com/example/todokotlin/controller/api/todo/TodoApiController.kt
+++ src/main/kotlin/com/example/todokotlin/controller/api/todo/TodoApiController.kt
@@ -1,30 +1,48 @@
 package com.example.todokotlin.controller.api.todo
 
 import com.example.todokotlin.model.http.TodoDto
+import com.example.todokotlin.service.TodoService
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/todo")
-class TodoApiController {
+class TodoApiController(val todoService: TodoService) {
 
     @GetMapping(path = [""])
-    fun read(@RequestParam(required = false) index: Int?){
+    fun read(@RequestParam(required = false) index: Int?): ResponseEntity<Any?> {
+        return index?.let {
+            todoService.read(it)
+        }?.let {
+            return ResponseEntity.ok(it)
+        }
+            ?: kotlin.run {
+                return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+                    .header(HttpHeaders.LOCATION, "/api/todo/all")
+                    .build()
+            }
+    }
 
+    @GetMapping(path = ["/all"])
+    fun readAll(index: Int?): MutableList<TodoDto> {
+        return todoService.readAll()
     }
 
     @PostMapping(path = [""])
-    fun create(@Valid @RequestBody todoDto: TodoDto){
+    fun create(@Valid @RequestBody todoDto: TodoDto) {
 
     }
 
     @PutMapping(path = [""])
-    fun update(@Valid @RequestBody todoDto: TodoDto){
+    fun update(@Valid @RequestBody todoDto: TodoDto) {
 
     }
 
     @DeleteMapping(path = ["/{index}"])
-    fun delete(@PathVariable(name = "index") _index: Int){
+    fun delete(@PathVariable(name = "index") _index: Int) {
 
     }
 }
